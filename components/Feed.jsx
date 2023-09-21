dd56b5b14be9aa7d51e1353a5230a6664f6247f8
mansiraizada <mansi23raizada@gmail.com>
2023-09-21 11:27:48 +0530

    New commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,19 +4,39 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({data, clickHandler}) => {
-
+    return (
+        <div className="mt-16 prompt_layout">
+            {data.map((prompt) => {
+                <PromptCard
+                 key={prompt._id}
+                 post={prompt}
+                 clickTagHandler={clickHandler}
+                />
+            })}
+        </div>
+    )
 };
 
 const Feed = () => {
     const [searchText, setSearchText] = useState("");
+    const [prompt, setPrompt] = useState([]);
 
     const searchChangeHandler = (e) => {
-
+        e.preventDefault();
+        setSearchText(e.target.value);
     }
 
     useEffect(() => {
-        
+        const fetchPosts = async () => {
+            const response = await fetch('/api/prompt');
+            const data = await response.json();
+
+            setPrompt(data);
+        }
+
+        fetchPosts();
     }, [])
+
     return (
         <section className="feed">
             <form className="relative w-full flex-center">
@@ -30,7 +50,7 @@ const Feed = () => {
             </form>
 
             <PromptCardList
-             data={[]}
+             data={prompt}
              clickHandler={() => {}}
             />
         </section>

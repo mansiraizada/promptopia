7f45e8bd7b72cb679ab7556fe2010dd86ffded42
mansiraizada <mansi23raizada@gmail.com>
2023-09-21 11:13:40 +0530

    New commmit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,39 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({data, clickHandler}) => {
+
+};
 
 const Feed = () => {
+    const [searchText, setSearchText] = useState("");
+
+    const searchChangeHandler = (e) => {
+
+    }
+
+    useEffect(() => {
+        
+    }, [])
     return (
-        <div>Feed</div>
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input
+                 className="search_input peer"
+                 type="text"
+                 placeholde r="Search for a tag or a username"
+                 value={searchText}
+                 onChange={searchChangeHandler}
+                 reuired/>
+            </form>
+
+            <PromptCardList
+             data={[]}
+             clickHandler={() => {}}
+            />
+        </section>
     )
 }
 
